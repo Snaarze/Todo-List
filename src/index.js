e21f780013a0b9bf61357eee5f72fdb9bcc72ccf
Snaarze <panlasiguijeremyglenn49@gmail.com>
2024-09-21 13:49:23 +0800

    added dispalycard to display the first project every time the page reloads

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
-import "./style.css"
+import "./style.css";
 import { addEventOnProjectList } from "./script/viewProject";
 import { addProjectBtn } from "./script/createProject";
+import { displayCard } from "./script/viewProject";
 
-function renderCardNotes(){
-    addProjectBtn();
-    // initilized to have eventListener
-    addEventOnProjectList();
+function renderCardNotes() {
+  addProjectBtn();
+  // initilized to have eventListener
+  addEventOnProjectList();
+  // initial default display card
+  displayCard(0);
 }
 
 // default project
