72cea246db3fb79b0775c4a63a577d5b14a7a738
Snaarze <panlasiguijeremyglenn49@gmail.com>
2024-09-21 13:53:22 +0800

    fixed indentation

--- src/script/createNewList.js
+++ src/script/createNewList.js
@@ -1,18 +1,24 @@
 import { displayCard } from "./viewProject";
 class list {
-    constructor(title,description, dueDate, priority, notes , checklist ){
-        this.title = title;
-        this.description = description;
-        this.dueDate = dueDate;
-        this.priority = priority;
-        this.notes = notes;
-        this.checklist = checklist
-    }
+  constructor(title, description, dueDate, priority, notes, checklist) {
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+    this.notes = notes;
+    this.checklist = checklist;
+  }
 }
 
-
-export const createList = (array,index) =>{
-    const createNewProject = new list(prompt("Title: "),prompt("Description: "),prompt("dueDate: "),prompt("Priority: "),prompt("Notes : "),prompt("Checklist: "))
-    array[index].list.push(createNewProject)
-    displayCard(index)
-}
+export const createList = (array, index) => {
+  const createNewProject = new list(
+    prompt("Title: "),
+    prompt("Description: "),
+    prompt("dueDate: "),
+    prompt("Priority: "),
+    prompt("Notes : "),
+    prompt("Checklist: ")
+  );
+  array[index].list.push(createNewProject);
+  displayCard(index);
+};
--- src/script/data.js
+++ src/script/data.js
@@ -1,60 +1,62 @@
- export const array = [
-    {
-        id : 0,
-        projectName : "Tracker",
-        list : [
-            {
-                id : 0,
-                title : "My Weight",
-                notes : "I need to reduce my weight by 2kg",
-                dueDate : "August 21, 2023",
-            },
-            {
-                id : 1,
-                title : "My Heigt",
-                notes : "I need to increase my height by 1 inch!",
-                dueDate : "August 21, 2023",
-            },
-            {
-                id : 2,
-                title : "Research",
-                notes : "I need to research about computers",
-                dueDate : "August 21, 2023",
-            },
-            {
-                id : 3,
-                title : "Assignment",
-                notes : "Math, English, Programming, Physical education",
-                dueDate : "August 21, 2023",
-            }, {
-                id : 4,
-                title : "Nothing",
-                notes : "Bluh",
-                dueDate : "August 21, 2023",
-            }, {
-                id : 5,
-                title : "Note sure",
-                notes : "say hi!",
-                dueDate : "August 21, 2023",
-            }
-        ]
-    },
-    {
-        id : 5,
-        projectName : "Tracker 2",
-        list : [
-            {
-                id : 0,
-                title : "My Weight",
-                notes : "I need to reduce my weight by 2kg",
-                dueDate : "August 21, 2023",
-            },
-            {
-                id : 1,
-                title : "My Heigt",
-                notes : "I need to increase my height by 1 inch!",
-                dueDate : "August 21, 2023",
-            }
-        ]
-    }
-]
+export const array = [
+  {
+    id: 0,
+    projectName: "Tracker",
+    list: [
+      {
+        id: 0,
+        title: "My Weight",
+        notes: "I need to reduce my weight by 2kg",
+        dueDate: "August 21, 2023",
+      },
+      {
+        id: 1,
+        title: "My Heigt",
+        notes: "I need to increase my height by 1 inch!",
+        dueDate: "August 21, 2023",
+      },
+      {
+        id: 2,
+        title: "Research",
+        notes: "I need to research about computers",
+        dueDate: "August 21, 2023",
+      },
+      {
+        id: 3,
+        title: "Assignment",
+        notes: "Math, English, Programming, Physical education",
+        dueDate: "August 21, 2023",
+      },
+      {
+        id: 4,
+        title: "Nothing",
+        notes: "Bluh",
+        dueDate: "August 21, 2023",
+      },
+      {
+        id: 5,
+        title: "Note sure",
+        notes: "say hi!",
+        dueDate: "August 21, 2023",
+      },
+    ],
+  },
+  {
+    id: 5,
+    projectName: "Tracker 2",
+    list: [
+      {
+        id: 0,
+        title: "My Weight",
+        notes: "I need to reduce my weight by 2kg",
+        dueDate: "August 21, 2023",
+      },
+      {
+        id: 1,
+        title: "My Heigt",
+        notes: "I need to increase my height by 1 inch!",
+        dueDate: "August 21, 2023",
+      },
+    ],
+  },
+];
