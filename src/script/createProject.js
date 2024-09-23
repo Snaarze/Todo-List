326e57ede99e81ceb0e73f600dd8a3884a323a97
Snaarze <panlasiguijeremyglenn49@gmail.com>
2024-09-23 18:02:30 +0800

    refactored to Project Class taht can create and add project

--- src/script/createProject.js
+++ src/script/createProject.js
@@ -1,61 +1,51 @@
 import { array } from "./data";
-import { addEventOnProjectList } from "./viewProject";
-import { displayCard } from "./viewProject";
+import { ProjectHandler } from "./viewProject";
+
+
 class Project {
   constructor(id, projectName) {
     this.id = id;
     this.projectName = projectName;
     this.list = [];
   }
-}
 
-const createProject = (array) => {
-  const currentIdProject =
+  getArrayIndex(id) {
+    return array.findIndex((element) => element.id === id);
+  }
+
+  getArrayName(currentId){
+    return array.findIndex((item) => item.id === currentId);
+  }
+
+  createProject(array) {
+    const currentIdProject =
     array.length === 0 ? 0 : array[array.length - 1].id + 1;
-  const createNewProject = new Project(currentIdProject, prompt());
-  array.push(createNewProject);
-  appendProject(currentIdProject, array);
+    const createNewProject = new Project(currentIdProject, prompt());
+    array.push(createNewProject);
+    ProjectHandler.renderedList();
 
-  // always display the newly created Project
-  const getCurrentIndex = array.findIndex(
-    (item) => item.id === currentIdProject
-  );
-  displayCard(getCurrentIndex);
+    // always display the newly created Project
+    const getCurrentIndex = this.getArrayName(currentIdProject);
+    ProjectHandler.displayCard(getCurrentIndex);
+    ProjectHandler.addEventOnProjectList();
+  }
 
-  addEventOnProjectList();
-  return currentIdProject;
-};
-
-function appendProject(id, array) {
-  const listContainer = document.querySelector(".list-container");
-  const getArrayName = array.findIndex((element) => element.id === id);
-  // create an li to append on the sidenav
-  const createLi = document.createElement("li");
-  const createSpan = document.createElement("span");
-  createLi.style.display = "flex";
-  createLi.setAttribute("data-index", `${id}`);
-  createSpan.textContent = array[getArrayName].projectName;
-  createLi.appendChild(createSpan);
-  listContainer.appendChild(createLi);
-
-  const createDiv = document.createElement("div");
-  createDiv.style.display = "flex";
-  createDiv.style.gap = "5px";
-  createLi.appendChild(createDiv);
-  const createEditBtn = document.createElement("button");
-  createEditBtn.classList.add("projectEditBtn");
-  createEditBtn.textContent = "Edit";
-  const createDeleteBtn = document.createElement("button");
-  createDeleteBtn.classList.add("projectDeleteBtn");
-  createDeleteBtn.textContent = "Delete";
-
-  createDiv.appendChild(createEditBtn);
-  createDiv.appendChild(createDeleteBtn);
+  addProjectBtn(){
+    const addbtn = document.querySelector(".add");
+    addbtn.addEventListener("click", () => {
+      const createNewProject = new Project;
+      createNewProject.createProject(array);
+    }); 
+  };
 }
 
-export const addProjectBtn = () => {
-  const addbtn = document.querySelector(".add");
-  addbtn.addEventListener("click", () => {
-    createProject(array);
-  });
-};
+
+export const ProjectClass = new Project()
+
+ 
+
+
+
+
+
+
