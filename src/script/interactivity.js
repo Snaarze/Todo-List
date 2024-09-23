5e41f28bd06601884f615aa8525f9b52f66f1d3c
Snaarze <panlasiguijeremyglenn49@gmail.com>
2024-09-23 18:03:33 +0800

    added new function that find the index of the array, refactored the edit and delete button

--- src/script/interactivity.js
+++ src/script/interactivity.js
@@ -1,24 +1,40 @@
-import { displayCard } from "./viewProject";
+import { ProjectHandler } from "./viewProject";
+import { array } from "./data";
+
+export const getSelectedList = (element) => {
+  const activeProject = element.parentElement.parentElement.getAttribute("data-index");
+  return  array.findIndex((element) => element.id === +activeProject )
+  }
+
 
 // delete the row of the edit
-export const editBtn = (id, content, array) => {
-  return function () {
-    const newProjectName = prompt();
-    // if the prompt is empty return
-    if (!newProjectName) {
-      // changes nothng
-      return;
-    }
-    // change the projectName of the array with the vavlue of prompt
-    const changeName = (array[id].projectName = newProjectName);
-    content.textContent = changeName;
-  };
+export const editBtn = (array) => {
+  const btnEdit = document.querySelectorAll(".projectEditBtn");
+  
+  btnEdit.forEach(element => {
+    element.addEventListener("click", ()=>{
+      const newProjectName = prompt();
+      // if the prompt is empty return
+      if (!newProjectName) {
+        // changes nothng
+        return;
+      }
+      // change the projectName of the array with the vavlue of prompt
+      const currentList = element.parentElement.previousSibling.firstChild
+      const changeName = (array[getSelectedList(element)].projectName = newProjectName);
+       currentList.textContent = changeName;
+      console.log(array)
+    })
+  })
 };
 
 // delete the row of the list
-export const deleteBtn = (id, createLi, array) => {
-  return function () {
-    const main = document.querySelector(".main-content");
+export const deleteBtn = (array) => {
+  const btnDelete = document.querySelectorAll(".projectDeleteBtn")
+
+  btnDelete.forEach(btn =>{
+      btn.addEventListener("click", ()=>{
+        const main = document.querySelector(".main-content");
     const deleteModal = document.querySelector(".deleteModal");
     const yesBtn = document.querySelector(".yesBtn");
     const noBtn = document.querySelector(".noBtn");
@@ -38,7 +54,7 @@ export const deleteBtn = (id, createLi, array) => {
         }
         return;
       }
-      displayCard(array.length - 1);
+      ProjectHandler.displayCard(array.length - 1);
     });
 
     noBtn.addEventListener("click", () => {
@@ -46,5 +62,6 @@ export const deleteBtn = (id, createLi, array) => {
       deleteModal.style.display = "none";
       return;
     });
-  };
+      })
+  })
 };
